5996c4de78b4c40d2010506cac5097cb8146ca69
naveen <kalanaveen55@gmail.com>
2022-02-18 16:14:54 +0530

    cors added

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 5500;
 
 app.use(express.static('public'));
-connectDB();
 app.use(express.json());
+connectDB();
+//cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:5500']
+  }
+  app.use(cors);
 // set view engine
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
